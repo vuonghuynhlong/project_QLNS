b2f7e0b439eb6744eee555b59b798e79d8b44d57
vuonghuynhlong <vuonghuynhlong@gmail.com>
2016-11-16 21:01:37 +0700

    L_161116_2101

---
+++ QuanLyNhanSu/DAO/DAO_CONNECT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.DAO
+{
+   
+    public class EMP_MANAGEMENTData
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString
+                    = "Data Source=.\\sqlexpress;Initial Catalog=EMP_MANAGEMENT;Integrated Security=SSPI;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+
+}
