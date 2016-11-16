3a3ef070daac8d526a50ae1fde6ab1eb780bf70b
vuonghuynhlong <vuonghuynhlong@gmail.com>
2016-11-16 16:47:38 +0700

    commit moi

--- QuanLyNhanSu/Program.cs
+++ QuanLyNhanSu/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyNhanSu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
